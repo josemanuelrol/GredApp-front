30b1415143fb552d66155fe6634b1dbc951cd7c0
josemanuelrol <josemanuelrol2004@gmail.com>
2024-05-13 21:25:13 +0200

    Subida del proyecto base

---
+++ GredApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GredApp',
+  webDir: 'www'
+};
+
+export default config;
